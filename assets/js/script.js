66ffaccf43e45b5fce324da9802e2ae415d630d1
MikeVez <mikevez14@gmail.com>
2020-06-05 11:13:25 +0000

    jQuery added to animate reels movement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,48 @@
-$("#spinBtn").click(function() {
-    var imgsA = $("#reels-container1 > div").remove().toArray();
-    for (var i = imgsA.length - 1; i >= 1; i--) {
-        var j = Math.floor(Math.random() * (i+1));
-        var imgsBi = imgsA[i];
-        var imgsBj = imgsA[j];
-        imgsA[i] = imgsBj;
-        imgsA[j] = imgsBi;
-    }
-    $("#reels-container1").append(imgsA);
+$(document).ready(function(){
+    $("#spinBtn").click(function() {
 
-    var imgsA = $("#reels-container2 > div").remove().toArray();
-    for (var i = imgsA.length - 1; i >= 1; i--) {
-        var j = Math.floor(Math.random() * (i+1));
-        var imgsBi = imgsA[i];
-        var imgsBj = imgsA[j];
-        imgsA[i] = imgsBj;
-        imgsA[j] = imgsBi;
-    }
-    $("#reels-container2").append(imgsA);
+        /*first reel*/
+        $("#reels-container1").animate({marginTop: "-=1000px"}, 200);
+        $("#reels-container1").animate({marginTop: "0"}, 200);
 
-    var imgsA = $("#reels-container3 > div").remove().toArray();
-    for (var i = imgsA.length - 1; i >= 1; i--) {
-        var j = Math.floor(Math.random() * (i+1));
-        var imgsBi = imgsA[i];
-        var imgsBj = imgsA[j];
-        imgsA[i] = imgsBj;
-        imgsA[j] = imgsBi;
-    }
-    $("#reels-container3").append(imgsA);
+        var imgsA = $("#reels-container1 > div").remove().toArray();
+        for (var i = imgsA.length - 1; i >= 1; i--) {
+            var j = Math.floor(Math.random() * (i+1));
+            var imgsBi = imgsA[i];
+            var imgsBj = imgsA[j];
+            imgsA[i] = imgsBj;
+            imgsA[j] = imgsBi;
+        }
+        $("#reels-container1").append(imgsA);
+   
+        
+        /*second reel*/
+        $("#reels-container2").animate({marginTop: "-=1000px"}, 200);
+        $("#reels-container2").animate({marginTop: "0"}, 200);
+
+        var imgsA = $("#reels-container2 > div").remove().toArray();
+        for (var i = imgsA.length - 1; i >= 1; i--) {
+            var j = Math.floor(Math.random() * (i+1));
+            var imgsBi = imgsA[i];
+            var imgsBj = imgsA[j];
+            imgsA[i] = imgsBj;
+            imgsA[j] = imgsBi;
+        }
+        $("#reels-container2").append(imgsA);
+    
+
+        /*third reel*/
+        $("#reels-container3").animate({marginTop: "-=1000px"}, 200);
+        $("#reels-container3").animate({marginTop: "0"}, 200);
+
+        var imgsA = $("#reels-container3 > div").remove().toArray();
+        for (var i = imgsA.length - 1; i >= 1; i--) {
+            var j = Math.floor(Math.random() * (i+1));
+            var imgsBi = imgsA[i];
+            var imgsBj = imgsA[j];
+            imgsA[i] = imgsBj;
+            imgsA[j] = imgsBi;
+        }
+        $("#reels-container3").append(imgsA);
+    });
 });
