93601deb5f73d9d563e240ce19502b9410483c10
MikeVez <mikevez14@gmail.com>
2020-06-06 18:38:01 +0000

    stop motion added to reels

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ $(document).ready(function(){
         /*first reel*/
         $("#reels-container1").animate({marginTop: "-=1000px"}, 200);
         $("#reels-container1").animate({marginTop: "0"}, 200);
+        $("#reels-container1").animate({marginTop: "-=10px"}, 200);
+        $("#reels-container1").animate({marginTop: "+=10px"}, 200);
+        $("#reels-container1").animate({marginTop: "0px"}, 200);
 
         var imgsA = $("#reels-container1 > div").remove().toArray();
         for (var i = imgsA.length - 1; i >= 1; i--) {
@@ -14,11 +17,18 @@ $(document).ready(function(){
             imgsA[j] = imgsBi;
         }
         $("#reels-container1").append(imgsA);
-   
-        
+        var reels1_div = document.getElementById("reels-container1");
+
+
+
         /*second reel*/
         $("#reels-container2").animate({marginTop: "-=1000px"}, 200);
         $("#reels-container2").animate({marginTop: "0"}, 200);
+        $("#reels-container2").animate({marginTop: "-=1000px"}, 200);
+        $("#reels-container2").animate({marginTop: "0"}, 200);
+        $("#reels-container2").animate({marginTop: "-=10px"}, 200);
+        $("#reels-container2").animate({marginTop: "+=10px"}, 200);
+        $("#reels-container2").animate({marginTop: "0px"}, 200);
 
         var imgsA = $("#reels-container2 > div").remove().toArray();
         for (var i = imgsA.length - 1; i >= 1; i--) {
@@ -29,11 +39,20 @@ $(document).ready(function(){
             imgsA[j] = imgsBi;
         }
         $("#reels-container2").append(imgsA);
-    
+        var reels2_div = document.getElementById("reels-container2");
+
+
 
         /*third reel*/
         $("#reels-container3").animate({marginTop: "-=1000px"}, 200);
         $("#reels-container3").animate({marginTop: "0"}, 200);
+        $("#reels-container3").animate({marginTop: "-=1000px"}, 200);
+        $("#reels-container3").animate({marginTop: "0"}, 200);
+        $("#reels-container3").animate({marginTop: "-=1000px"}, 200);
+        $("#reels-container3").animate({marginTop: "0"}, 200);
+        $("#reels-container3").animate({marginTop: "-=10px"}, 200);
+        $("#reels-container3").animate({marginTop: "+=10px"}, 200);
+        $("#reels-container3").animate({marginTop: "0px"}, 200);
 
         var imgsA = $("#reels-container3 > div").remove().toArray();
         for (var i = imgsA.length - 1; i >= 1; i--) {
@@ -44,11 +63,22 @@ $(document).ready(function(){
             imgsA[j] = imgsBi;
         }
         $("#reels-container3").append(imgsA);
+        var reels3_div = document.getElementById("reels-container3");
+
 
 
         /*fourth reel*/
         $("#reels-container4").animate({marginTop: "-=1000px"}, 200);
         $("#reels-container4").animate({marginTop: "0"}, 200);
+        $("#reels-container4").animate({marginTop: "-=1000px"}, 200);
+        $("#reels-container4").animate({marginTop: "0"}, 200);
+        $("#reels-container4").animate({marginTop: "-=1000px"}, 200);
+        $("#reels-container4").animate({marginTop: "0"}, 200);
+        $("#reels-container4").animate({marginTop: "-=1000px"}, 200);
+        $("#reels-container4").animate({marginTop: "0"}, 200);
+        $("#reels-container4").animate({marginTop: "-=10px"}, 200);
+        $("#reels-container4").animate({marginTop: "+=10px"}, 200);
+        $("#reels-container4").animate({marginTop: "0px"}, 200);
 
         var imgsA = $("#reels-container4 > div").remove().toArray();
         for (var i = imgsA.length - 1; i >= 1; i--) {
@@ -59,5 +89,11 @@ $(document).ready(function(){
             imgsA[j] = imgsBi;
         }
         $("#reels-container4").append(imgsA);
+        var reels4_div = document.getElementById("reels-container4");
+
+        console.log(reels1_div);
+        console.log(reels2_div);
+        console.log(reels3_div);
+        console.log(reels4_div);
     });
 });
